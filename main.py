d1f187c33566be38ed97af52692c67fe9587ae8f
MrHup <hup.hubspace@gmail.com>
2024-10-28 01:41:33 +0200

    Go back to explicit domains as it is required by socket io

--- main.py
+++ main.py
@@ -26,7 +26,14 @@ async def terms_of_service():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:8080",
+        "https://dashboard.twitch.tv/",
+        "https://*.twitch.tv/",
+        "https://*.ext-twitch.tv",
+        "https://a7x4e30v7yvxn69xwyrjstxzjuc1go.ext-twitch.tv/",
+        "https://twitch.tv",
+    ],
     allow_credentials=True,
     allow_methods=["*"],  # allow all HTTP methods
     allow_headers=["*"],  # allow all headers
